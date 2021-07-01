6757399149509ba9626c98d2865044420e25cca2
Zaldana <rooben.zaldana@gmail.com>
2021-07-01 14:57:25 -0400

    complete

---
+++ main.js
@@ -0,0 +1,30 @@
+const h1 = document.querySelector('h1');
+
+h1.style.fontSize = '48px';
+
+const paragraph = document.querySelector('p');
+
+paragraph.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+paragraph.style.color = 'lightblue';
+
+const item13 = document.querySelector('#item-13');
+
+item13.style.color = "rgba(170, 187, 97, 0.5)";
+
+const item3 = document.querySelector('#item-3');
+
+item3.innerText = "Hi!";
+
+const image = document.querySelector('img');
+
+image.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg';
+image.style.height = '300px';
+
+const image2 = document.querySelector('#image2');
+image2.style.height = '300px';
+
+
+const item16 = document.querySelector('#item-16');
+item16.className = "item";
+item16.innerText = "Won't get fooled again.";
